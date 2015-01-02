f78c75d7096ba601f3ec0338b360a10cc2f88f36
Michael Dvorkin <mike@dvorkin.net>
2015-01-02 00:12:40 -0800

    Square distance initialization

--- src/init.rs
+++ src/init.rs
@@ -1,12 +1,31 @@
 // Copyright (c) 2014-2015 by Michael Dvorkin. All Rights Reserved.
 
-use data::{A1, H8};
+use std::cmp::max;
+use std::num::SignedInt;
+use utils::{row, col, coordinate};
+
+// Distance between two squares.
+static mut _distance: [[uint, ..64], ..64] = [[0, ..64], ..64];
+
+#[inline(always)]
+pub fn distance(from: uint, to: uint) -> uint {
+    unsafe { _distance[from][to] }
+}
 
 pub fn init() {
+
     fn init_masks() {
-        for sq in range(A1, H8) {
-            sq == sq;
+        for sq in range(0, 64) {
+            let (row, col) = coordinate(sq);
+
+            for i in range(0, 64) {
+                let (r, c) = coordinate(i);
+                unsafe {
+                    _distance[sq][i] = max(((row - r) as int).abs(), ((col - c) as int).abs()) as uint;
+                }
+            }
         }
+
         println!("init_masks...");
     }
 
