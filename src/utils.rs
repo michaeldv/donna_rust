53d8795fb18e79b9251ca7ed24a38df4c250ac88
Michael Dvorkin <mike@dvorkin.net>
2015-01-12 21:59:27 -0800

    Fixed indentation

--- src/utils.rs
+++ src/utils.rs
@@ -2,15 +2,15 @@
 
 // Returns row number in 0..7 range for the given square.
 pub fn row(square: uint) -> uint {
-	square >> 3
+    square >> 3
 }
 
 // Returns column number in 0..7 range for the given square.
 pub fn col(square: uint) -> uint {
-	square & 7
+    square & 7
 }
 
 // Returns both row and column numbers for the given square.
 pub fn coordinate(square: uint) -> (uint, uint) {
-	(row(square), col(square))
+    (row(square), col(square))
 }
